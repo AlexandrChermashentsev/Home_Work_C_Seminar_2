f517f5e3d281ae408a05e0313d5a61ad1cbabbdc
Alexandr.Cherm <Alexandr.chermashentsev@gmail.com>
2022-10-19 18:40:03 +0500

    Add Ex 13 (not working) and Add Ex 15(working)

---
+++ Exercise_15/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Enter the number of the day of the week");
+string numberStr = Console.ReadLine();
+int numberWeek = Convert.ToInt32(numberStr);
+
+if (numberWeek < 1 || numberWeek > 7) Console.WriteLine("there is no such day, joker");
+else
+{
+    if (numberWeek == 6 || numberWeek == 7) Console.WriteLine("This day is a holiday");
+    else Console.WriteLine("This day is weekday");
+}
