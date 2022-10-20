d4fec6e71e5dfb12120b149c844a25b264623d32
Alexandr.Cherm <Alexandr.chermashentsev@gmail.com>
2022-10-20 11:58:38 +0500

    Add Ex 13 (Working)

--- Exercise_13/Program.cs
+++ Exercise_13/Program.cs
@@ -1,5 +1,4 @@
-﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
-или сообщает, что третьей цифры нет.
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 645 -> 5
 
@@ -9,65 +8,18 @@
 */
 
 Console.WriteLine("Enter the number");
-string numberStr = Console.ReadLine().Length;
+string numberStr = Console.ReadLine();
 int number = Convert.ToInt32(numberStr);
-int numberThird = 0;
-int index = 0;
-int position = 1;
-int anyNumber = number;
-
-if (number <100 && number > -100) Console.WriteLine("Третьей цифры нет");
+int count = number.ToString().Length;
+int result = 0;
+if (count < 3) Console.WriteLine("It's not a three digit number");
 else
 {
-    while (number > 0)
+    int degree = 1;
+    for (int index = count; index > 3; index--)
     {
-        number = number / 10;
-        position = position * 10;
-        index++;
+        degree = degree * 10;
     }
-    anyNumber = anyNumber % (position / 100);
-    int i = Convert.ToInt32(Math.Pow(10, index-3));
-    numberThird = anyNumber / (position / i);
-    Console.WriteLine($"index = {index}");
-    Console.WriteLine(anyNumber);
-    Console.WriteLine($"i = {i}");
-    Console.WriteLine($"Third digit = {numberThird}");
-}
-
-
-/*int number = ReadInt("Введите число: ");
-int count = number.ToString().Length;
-Console.Write(MakeArray(number, count));
-
-
-// ФУНКЦИИ------------------------------------------------------------------------------------------------------
-
-// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. 
-//Если 3 цифры нет, сообщает и выводит 0.
-int MakeArray(int a, int b)
-{
-int result = 0;
-    if (b < 3)
-    {
-        Console.Write("Третьей цифры нет, держи: ");
+    result = (number / degree) % 10;    
     }
-    else
-    {
-        int c = 1;
-        for (int i = b; i > 3; i--)
-        {
-            c = c * 10;
-        }
-
-        result = (a / c) % 10;
-    }
-return result;
-}
-*/
+Console.WriteLine($"Third digit = {result}");
